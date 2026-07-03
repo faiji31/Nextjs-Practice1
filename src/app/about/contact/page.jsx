59ff76614fd4f7ed52c35db8f4eae819d52a4f6f
Faiji Akbar Liam <faijiakbarliam2016@gmail.com>
2026-07-03 10:13:52 +0600

    3rd commit

---
+++ src/app/about/contact/page.jsx
@@ -0,0 +1,12 @@
+import Title from '@/components/Title'
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+      <Title>contact:01790528911</Title>
+    </div>
+  )
+}
+
+export default Contact
---
+++ src/app/about/teams/page.jsx
@@ -0,0 +1,12 @@
+import Title from '@/components/Title'
+import React from 'react'
+
+const Team = () => {
+  return (
+    <div>
+      <Title>Welcome to Team:1340+</Title>
+    </div>
+  )
+}
+
+export default Team
